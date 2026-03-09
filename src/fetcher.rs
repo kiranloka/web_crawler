a17b6902e91de6845c3fb900b02f53ecc2828f53
kiranLoka <kirankumarloka@gmail.com>
2026-03-10 00:06:15 +0530

    fetcher worker job

---
+++ src/fetcher.rs
@@ -0,0 +1,29 @@
+use crate::frontier::CrawlTask;
+use anyhow::Result;
+use reqwest::Client;
+use tokio::time::{sleep, Duration};
+
+#[derive(Clone)]
+pub struct Fetcher {
+    client: Client,
+    delay_ms: u64,
+}
+
+impl Fetcher {
+    pub fn new(delay_ms: u64) -> Self {
+        let client = Client::builder()
+            .user_agent("RustCrawler/0.1")
+            .build()
+            .unwrap();
+
+        Self { client, delay_ms }
+    }
+
+    pub async fn fetch(&self, task: CrawlTask) -> Result<String> {
+        sleep(Duration::from_millis(self.delay_ms)).await;
+
+        let resp = self.client.get(task_url.as_str()).send().await?;
+        let body = resp.text().await?;
+        Ok(body)
+    }
+}
